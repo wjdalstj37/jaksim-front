1b2590aa7d8890c59308468ffe49c65035b87767
wjdalstj37 <jms6020@gmail.com>
2022-08-19 23:13:56 +0900

    feat: sns Login success

--- src/pages/SnsAuthPage.js
+++ src/pages/SnsAuthPage.js
@@ -3,7 +3,6 @@ import { check } from "../modules/user";
 import { useDispatch } from "react-redux";
 
 const SnsAuthPage = () => {
-  console.log("아무거나");
   const dispatch = useDispatch();
   let accessToken = new URL(window.location.href).searchParams.get(
     "accessToken"
@@ -14,7 +13,7 @@ const SnsAuthPage = () => {
   localStorage.setItem("accessToken", accessToken);
   localStorage.setItem("refreshToken", refreshToken);
   client.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-  // dispatch(check()).then((response) => console.log(response));
+  dispatch(check());
 
   return <div>sns로그인</div>;
 };
